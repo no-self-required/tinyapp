f378f2e332a659328e10576fc0ebd8c139378a77
Louis Manlangit <louism.coding@gmail.com>
2021-08-03 02:23:47 +0000

    added tinyURL form

--- express_server.js
+++ express_server.js
@@ -1,8 +1,15 @@
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
+
+
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -11,6 +18,10 @@ const urlDatabase = {
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get('/urls', (req, res) => {
@@ -34,3 +45,22 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+function generateRandomString() {
+  let generated = [];
+  let stringy = 'abcdefghijklmnopqrtuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let joined = stringy.split('')
+  // console.log(random)
+  for (let i = 0; i < 6; i++) {
+    let random = joined[Math.floor(Math.random() * joined.length)];
+    generated.push(random)
+  }
+  return generated.join('')
+}
+
+
